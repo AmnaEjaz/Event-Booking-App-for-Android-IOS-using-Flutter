d2b227e2eac4fa54123a6fe419ebdf8c249d154a
Amna Ejaz <amna.ejaz@techlogix.com>
2020-05-26 15:14:24 +0500

    Update nat_event_api_provider.dart

--- darkarts/lib/services/nat_event_api_provider.dart
+++ darkarts/lib/services/nat_event_api_provider.dart
@@ -39,38 +39,14 @@ import 'package:http/http.dart' as http;
 class EventsApiProvider {
   final successCode = 200;
 
-  //for office
-  // Future<List<Event>> getAllEvents() async {
-  //   final response =
-  //       await http.get("http://172.17.40.196:6073/api/FeaturedEvents");
-  //   return parseResponse(response);
-  //   // return Future.delayed(Duration(seconds: 0), () => parseArray(mockEvents));
-  // }
-  //http://172.17.40.196:6072/api/Customer/1/LikeEvent/7/LIKE
+
 
 // for home
   Future<List<Event>> getAllEvents() async {
     return Future.delayed(Duration(seconds: 0), () => parseArray(mockEvents));
   }
 
-  // Future<bool> toggleEventLike(String eventCode, int customerId) async {
-  //   var params = {"EventCode": eventCode, "CustomerId": customerId};
-  //   Uri uri =
-  //       Uri.parse("http://172.17.40.196:6072/api/Customer/1/LikeEvent/7/LIKE");
-  //   final newURI = uri.replace(queryParameters: params);
-  //   final response = await http.post(newURI, body: json.encode(params));
-  //   final responseString = jsonDecode(response.body);
-  //   if (response.statusCode == successCode) {
-  //     if(responseString["data"] == true){
-  //     return true;
-  //     }
-  //     else{
-  //       return false;
-  //     }
-  //   } else {
-  //     throw Exception('failed to load events');
-  //   }
-  // }
+
 
   List<Event> parseResponse(http.Response response) {
     final responseString = jsonDecode(response.body);
